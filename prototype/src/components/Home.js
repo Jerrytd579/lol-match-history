0727180358ed87c71cd3daf5e14ee042687fbd36
rtrshm <ashum@stevens.edu>
2021-11-22 16:11:21 -0500

    hid api key

---
+++ prototype/src/components/Home.js
@@ -0,0 +1,18 @@
+import { React } from 'react';
+ 
+const handleSubmit = (event) => {
+
+}
+
+const SummonerSearch = () => {
+
+}
+
+const Home = () => {
+    return <div className="home">
+        <div id="summonerSearch">
+        </div>
+    </div>;
+};
+
+export default Home;
