909afb9ddc66206f8dd289f27f5c071f1b95ab7c
rtrshm <ashum@stevens.edu>
2021-11-22 15:45:24 -0500

    Revert "add basic data functions" git has not been my friend recently.. had some hiccups with moving prototype to a separate branch, it should be ironed out now This reverts commit 8dd5742a1db2d1130e4cedf971b884d699e64c37.

--- prototype/src/util.js
+++
@@ -1,9 +0,0 @@
-const {PlatformId} = require('@fightmegg/riot-api')
- 
-module.exports = {
-    strValid: str => str && typeof str === 'string' 
-        && str.replaceAll(/ +/g, '') !== '',
-    // ensure region exists and is a valid PlatformId value
-    regValid: reg => reg && typeof reg === 'string'
-        && ((Object.values(PlatformId).indexOf(reg)) > -1) 
-}
